9e0ce6865737f43531c3293a042e8317ba28ffee
sleshatuladhar <sleshatuladhar@gmail.com>
2023-08-28 10:20:41 +1000

    Added Scanner to CircleArea class

--- CircleArea.java
+++ CircleArea.java
@@ -5,10 +5,12 @@
  * @author (Slesha)
  * @version (28/08/2023)
  */
+
+import java.util.Scanner;
 public class CircleArea
 {
     // instance variables - replace the example below with your own
-    private double radius = 2.5;
+    //private double radius;
     private double area;
 
     /**
@@ -20,8 +22,11 @@ public class CircleArea
 
     public static void main(String[] args)
     {
+        Scanner input = new Scanner(System.in);
+        double radius = input.nextDouble();
+        
         CircleArea circle1 = new CircleArea();
-        circle1.getArea();
+        circle1.getArea(radius);
     }
     
     /**
@@ -30,9 +35,9 @@ public class CircleArea
      * @param  radius  Radius of the circle
      * @return    the radius of circle
      */
-    public void getArea()
+    public void getArea(double radius)
     {
-        this.area = this.radius * this.radius * 3.14;
+        this.area = radius * radius * 3.14;
         System.out.println("The area of the circle = " + this.area);
     }
 }
